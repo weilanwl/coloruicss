e85f3bc3ff06f7d8b797979e500a652fccf87207
Weilanwl <1273753708@qq.com>
2019-01-06 22:59:40 +0800

    v2.0.3
    1.修复一些单位错误（带输入框的操作条）
    2.纠正一些单词拼写...
    3.抛弃icon标签，改回text标签的写法
    4.抛弃px单位的样式文件
    5.优化一些组件的字体大小
    6.新增两种加载样式

--- demo/pages/basics/loading/loading.js
+++ demo/pages/basics/loading/loading.js
@@ -2,11 +2,36 @@ const app = getApp();
 Page({
   data: {
     StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar
+    CustomBar: app.globalData.CustomBar,
+    loadProgress:0
   },
   isLoading (e) {
     this.setData({
       isLoad: e.detail.value
     })
   },
+  loadModal () {
+    this.setData({
+      loadModal: true
+    })
+    setTimeout(()=> {
+      this.setData({
+        loadModal: false
+      })
+    }, 2000)
+  },
+  loadProgress(){
+    this.setData({
+      loadProgress: this.data.loadProgress+3
+    })
+    if (this.data.loadProgress<100){
+      setTimeout(() => {
+        this.loadProgress();
+      }, 100)
+    }else{
+      this.setData({
+        loadProgress: 0
+      })
+    }
+  }
 });
