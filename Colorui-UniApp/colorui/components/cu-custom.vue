85c4809a44331aa55e03a1d34483c549d17da452
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2019-04-12 11:08:50 +0800

    分享出去的小程序卡片点开，无pages栈，兼容代码回到首页

--- Colorui-UniApp/colorui/components/cu-custom.vue
+++ Colorui-UniApp/colorui/components/cu-custom.vue
@@ -52,6 +52,10 @@
 		},
 		methods: {
 			BackPage() {
+				if (getCurrentPages().length < 2 && 'undefined' !== typeof __wxConfig) {
+					let url = '/' + __wxConfig.pages[0]
+					return uni.redirectTo({url})
+				}
 				uni.navigateBack({
 					delta: 1
 				});
